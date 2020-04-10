e901dab14b189610c77dc3c7dfaaa351892b99dc
Kevin Yang <kyang@lyft.com>
2020-04-09 22:47:23 -0700

    make cronjobController sync period configurable via flag
    The cronjob controller syncs the world every 10 seconds. We'd like to make this configurable since when the world gets big (lots of Crons), this sync time becomes effectively an unnecessary sleep.
    
    * add syncLoopPeriod parameter to CronJobController
    
    * make all the config changes everywhere
    
    * add autogenerated files

--- cmd/kube-controller-manager/app/options/options_test.go
+++ cmd/kube-controller-manager/app/options/options_test.go
@@ -33,6 +33,7 @@ import (
 	kubecontrollerconfig "k8s.io/kubernetes/cmd/kube-controller-manager/app/config"
 	kubectrlmgrconfig "k8s.io/kubernetes/pkg/controller/apis/config"
 	csrsigningconfig "k8s.io/kubernetes/pkg/controller/certificates/signer/config"
+	cronjobconfig "k8s.io/kubernetes/pkg/controller/cronjob/config"
 	daemonconfig "k8s.io/kubernetes/pkg/controller/daemon/config"
 	deploymentconfig "k8s.io/kubernetes/pkg/controller/deployment/config"
 	endpointconfig "k8s.io/kubernetes/pkg/controller/endpoint/config"
@@ -145,6 +146,87 @@ func TestAddFlags(t *testing.T) {
 		fs.AddFlagSet(f)
 	}
 
+	args := []string{
+		"--address=192.168.4.10",
+		"--allocate-node-cidrs=true",
+		"--attach-detach-reconcile-sync-period=30s",
+		"--cidr-allocator-type=CloudAllocator",
+		"--cloud-config=/cloud-config",
+		"--cloud-provider=gce",
+		"--cluster-cidr=1.2.3.4/24",
+		"--cluster-name=k8s",
+		"--cluster-signing-cert-file=/cluster-signing-cert",
+		"--cluster-signing-key-file=/cluster-signing-key",
+		"--concurrent-deployment-syncs=10",
+		"--concurrent-statefulset-syncs=15",
+		"--concurrent-endpoint-syncs=10",
+		"--concurrent-service-endpoint-syncs=10",
+		"--concurrent-gc-syncs=30",
+		"--concurrent-namespace-syncs=20",
+		"--concurrent-replicaset-syncs=10",
+		"--concurrent-resource-quota-syncs=10",
+		"--concurrent-service-syncs=2",
+		"--concurrent-serviceaccount-token-syncs=10",
+		"--concurrent_rc_syncs=10",
+		"--configure-cloud-routes=false",
+		"--contention-profiling=true",
+		"--controller-start-interval=2m",
+		"--controllers=foo,bar",
+		"--cronjob-controller-sync-period=10s",
+		"--deployment-controller-sync-period=45s",
+		"--disable-attach-detach-reconcile-sync=true",
+		"--enable-dynamic-provisioning=false",
+		"--enable-garbage-collector=false",
+		"--enable-hostpath-provisioner=true",
+		"--enable-taint-manager=false",
+		"--experimental-cluster-signing-duration=10h",
+		"--flex-volume-plugin-dir=/flex-volume-plugin",
+		"--horizontal-pod-autoscaler-downscale-delay=2m",
+		"--horizontal-pod-autoscaler-sync-period=45s",
+		"--horizontal-pod-autoscaler-upscale-delay=1m",
+		"--horizontal-pod-autoscaler-downscale-stabilization=3m",
+		"--horizontal-pod-autoscaler-cpu-initialization-period=90s",
+		"--horizontal-pod-autoscaler-initial-readiness-delay=50s",
+		"--http2-max-streams-per-connection=47",
+		"--kube-api-burst=100",
+		"--kube-api-content-type=application/json",
+		"--kube-api-qps=50.0",
+		"--kubeconfig=/kubeconfig",
+		"--large-cluster-size-threshold=100",
+		"--leader-elect=false",
+		"--leader-elect-lease-duration=30s",
+		"--leader-elect-renew-deadline=15s",
+		"--leader-elect-resource-lock=configmap",
+		"--leader-elect-retry-period=5s",
+		"--master=192.168.4.20",
+		"--max-endpoints-per-slice=200",
+		"--min-resync-period=8h",
+		"--namespace-sync-period=10m",
+		"--node-cidr-mask-size=48",
+		"--node-eviction-rate=0.2",
+		"--node-monitor-grace-period=30s",
+		"--node-monitor-period=10s",
+		"--node-startup-grace-period=30s",
+		"--pod-eviction-timeout=2m",
+		"--port=10000",
+		"--profiling=false",
+		"--pv-recycler-increment-timeout-nfs=45",
+		"--pv-recycler-minimum-timeout-hostpath=45",
+		"--pv-recycler-minimum-timeout-nfs=200",
+		"--pv-recycler-timeout-increment-hostpath=45",
+		"--pvclaimbinder-sync-period=30s",
+		"--resource-quota-sync-period=10m",
+		"--route-reconciliation-period=30s",
+		"--secondary-node-eviction-rate=0.05",
+		"--service-account-private-key-file=/service-account-private-key",
+		"--terminated-pod-gc-threshold=12000",
+		"--unhealthy-zone-threshold=0.6",
+		"--use-service-account-credentials=true",
+		"--cert-dir=/a/b/c",
+		"--bind-address=192.168.4.21",
+		"--secure-port=10001",
+		"--concurrent-ttl-after-finished-syncs=8",
+	}
 	fs.Parse(args)
 	// Sort GCIgnoredResources because it's built from a map, which means the
 	// insertion order is random.
@@ -214,6 +296,11 @@ func TestAddFlags(t *testing.T) {
 				ClusterSigningCertFile: "/cluster-signing-cert",
 				ClusterSigningKeyFile:  "/cluster-signing-key",
 				ClusterSigningDuration: metav1.Duration{Duration: 10 * time.Hour},
+			},
+		},
+		CronJobController: &CronJobControllerOptions{
+			&cronjobconfig.CronJobControllerConfiguration{
+				CronJobControllerSyncPeriod: metav1.Duration{Duration: 10 * time.Second},
 			},
 		},
 		DaemonSetController: &DaemonSetControllerOptions{
