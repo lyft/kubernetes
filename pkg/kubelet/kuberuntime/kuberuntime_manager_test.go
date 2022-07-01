5bca6e2971e751a3ddc22b9dfff40d39bb588dc4
Camille Chen <camillechen@lyft.com>
2022-06-30 17:20:33 -0700

    remove outdated import libraries and change the outdated functions

--- pkg/kubelet/kuberuntime/kuberuntime_manager_test.go
+++ pkg/kubelet/kuberuntime/kuberuntime_manager_test.go
@@ -1575,7 +1575,7 @@ func TestComputePodActionsWithSidecar(t *testing.T) {
 			mutateStatusFn: func(status *kubecontainer.PodStatus) {
 				// No container or sandbox exists.
 				status.SandboxStatuses = []*runtimeapi.PodSandboxStatus{}
-				status.ContainerStatuses = []*kubecontainer.ContainerStatus{}
+				status.ContainerStatuses = []*kubecontainer.Status{}
 			},
 			actions: podActions{
 				KillPod:           true,
