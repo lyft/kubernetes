31a72ace90be9f748d46ab114f052932d92c7a8c
Abhinav Dahiya <adahiya@lyft.com>
2023-04-20 20:23:21 -0400

    handle case where there is no known container state
    Based on the api documentation
    https://pkg.go.dev/k8s.io/api/core/v1#ContainerState
    ```
    ContainerState holds a possible state of container. Only one of its members may be specified.
    If none of them is specified, the default one is ContainerStateWaiting.
    ```
    
    There is a case where no explicit state is known for a container and it must be
    treated as waiting state.
    
    So we must check for nil value (since the State is a value and pointer we have to check
    that all possible fields are nil , :( ) and treat it as Waiting state.
    
    Also fixes the `Restart running non-sidecars despite sidecar becoming not ready` test.
    A pod if needs to be restarted (i.e it had existed and has run at least one) the state
    of the container should be set to last running state.
    
    compare to zero value

--- pkg/kubelet/kuberuntime/kuberuntime_manager_test.go
+++ pkg/kubelet/kuberuntime/kuberuntime_manager_test.go
@@ -1620,6 +1620,33 @@ func TestComputePodActionsWithSidecar(t *testing.T) {
 				ContainersToKill:  getKillMap(basePod, baseStatus, []int{}),
 			},
 		},
+		"Don't start non-sidecars until sidecars are ready, no known previous container state": {
+			mutatePodFn: func(pod *v1.Pod) {
+				pod.Status.ContainerStatuses = []v1.ContainerStatus{
+					{
+						Name:  "foo1",
+						State: v1.ContainerState{},
+					},
+					{
+						Name:  "foo2",
+						Ready: false,
+					},
+				}
+			},
+			mutateStatusFn: func(status *kubecontainer.PodStatus) {
+				for i := range status.ContainerStatuses {
+					if i == 1 {
+						continue
+					}
+					status.ContainerStatuses[i].State = ""
+				}
+			},
+			actions: podActions{
+				SandboxID:         baseStatus.SandboxStatuses[0].Id,
+				ContainersToStart: []int{},
+				ContainersToKill:  getKillMap(basePod, baseStatus, []int{}),
+			},
+		},
 		"Start non-sidecars when sidecars are ready": {
 			mutatePodFn: func(pod *v1.Pod) {
 				pod.Status.ContainerStatuses = []v1.ContainerStatus{
@@ -1697,6 +1724,9 @@ func TestComputePodActionsWithSidecar(t *testing.T) {
 				pod.Status.ContainerStatuses = []v1.ContainerStatus{
 					{
 						Name: "foo1",
+						State: v1.ContainerState{
+							Running: &v1.ContainerStateRunning{StartedAt: metav1.Now()},
+						},
 					},
 					{
 						Name:  "foo2",
@@ -1704,6 +1734,9 @@ func TestComputePodActionsWithSidecar(t *testing.T) {
 					},
 					{
 						Name: "foo3",
+						State: v1.ContainerState{
+							Running: &v1.ContainerStateRunning{StartedAt: metav1.Now()},
+						},
 					},
 				}
 			},
