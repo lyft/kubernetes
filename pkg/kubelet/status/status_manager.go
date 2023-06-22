lyft/kubernetes

commit e698aaa287421d7d2e55f0f141b5dc5e18a6924b

container ordering: Don't run application container when container status is not available (#37)
1. When all container statuses are nil, we should default to application
containers wait untill statuses are available

2. When application container is nil, we should default to application
containers waiting state

pkg/kubelet/status/status_manager.go
@@ -896,17 +896,23 @@ type SidecarsStatus struct {
 	ContainersWaiting bool
 }
 
+// NotEnoughInformationSidecarStatus should be used when we don't have all the information
+// to get the complete status.
+// This state prevents application container from making progress
+// but sidecars should always be allowed to progress.
+var NotEnoughInformationSidecarStatus = SidecarsStatus{SidecarsPresent: true, SidecarsReady: false, ContainersWaiting: true}
+
 // GetSidecarsStatus returns the SidecarsStatus for the given pod
 func GetSidecarsStatus(pod *v1.Pod) SidecarsStatus {
 	if pod == nil {
 		klog.Infof("Pod was nil, returning empty sidecar status")
-		return SidecarsStatus{}
+		return NotEnoughInformationSidecarStatus
 	}
 	if pod.Spec.Containers == nil || pod.Status.ContainerStatuses == nil {
 		klog.Infof("Pod Containers or Container status was nil, returning empty sidecar status")
-		return SidecarsStatus{}
+		return NotEnoughInformationSidecarStatus
 	}
-	sidecarsStatus := SidecarsStatus{SidecarsPresent: false, SidecarsReady: true, ContainersWaiting: false}
+	sidecarsStatus := SidecarsStatus{SidecarsPresent: false, SidecarsReady: true, ContainersWaiting: true}
 	for _, container := range pod.Spec.Containers {
 		for _, status := range pod.Status.ContainerStatuses {
 			if status.Name == container.Name {
@@ -921,9 +927,12 @@ func GetSidecarsStatus(pod *v1.Pod) SidecarsStatus {
 				} else {
 					if (status.State == v1.ContainerState{}) || // For zero value, the default state is ContainerStateWaiting.
 						status.State.Waiting != nil {
-						// check if non-sidecars have started
+						// non-sidecar is waiting
 						klog.Infof("Pod: %s: %s: non-sidecar waiting", format.Pod(pod), container.Name)
-						sidecarsStatus.ContainersWaiting = true
+					} else {
+						// non-sidecar has started running
+						klog.Infof("Pod: %s: %s: non-sidecar started", format.Pod(pod), container.Name)
+						sidecarsStatus.ContainersWaiting = false
 					}
 				}
 			}

pkg/kubelet/kuberuntime/kuberuntime_manager_test.go
@@ -1562,7 +1562,30 @@ func TestComputePodActionsWithSidecar(t *testing.T) {
 				ContainersToKill:  getKillMap(basePod, baseStatus, []int{}),
 			},
 		},
-		"Don't start non-sidecars until sidecars are ready, no known previous container state": {
+		"Don't start non-sidecars until sidecars are ready, no known previous container state for all non-sidecar": {
+			mutatePodFn: func(pod *v1.Pod) {
+				pod.Status.ContainerStatuses = []v1.ContainerStatus{
+					{
+						Name:  "foo2",
+						Ready: false,
+					},
+				}
+			},
+			mutateStatusFn: func(status *kubecontainer.PodStatus) {
+				for i := range status.ContainerStatuses {
+					if i == 1 {
+						continue
+					}
+					status.ContainerStatuses[i].State = ""
+				}
+			},
+			actions: podActions{
+				SandboxID:         baseStatus.SandboxStatuses[0].Id,
+				ContainersToStart: []int{},
+				ContainersToKill:  getKillMap(basePod, baseStatus, []int{}),
+			},
+		},
+		"Don't start non-sidecars until sidecars are ready, no known previous container state for one non-sidecar": {
 			mutatePodFn: func(pod *v1.Pod) {
 				pod.Status.ContainerStatuses = []v1.ContainerStatus{
 					{

commit 49b8055caec6891f9d95c3fc4b1608b68919e5f0

containers should always be allowed to start when no sidecar is present (#40)
based on
https://lyft.slack.com/archives/C017X524VC7/p1685639606706689?thread_ts=1685481685.730389&cid=C017X524VC7

when there are no sidecars we should allow all containers to start even
when there is lack of container status.
This fixes a regression introduced in
https://github.com/lyft/kubernetes/pull/37 where we would block starting
containers until we get container status reported. this triggers KillPod
action in 1.16 as no progress can be made.

---------

Co-authored-by: Tom Wanielista <tomwans@users.noreply.github.com>

pkg/kubelet/status/status_manager.go
@@ -908,10 +908,21 @@ func GetSidecarsStatus(pod *v1.Pod) SidecarsStatus {
 		klog.Infof("Pod was nil, returning empty sidecar status")
 		return NotEnoughInformationSidecarStatus
 	}
-	if pod.Spec.Containers == nil || pod.Status.ContainerStatuses == nil {
-		klog.Infof("Pod Containers or Container status was nil, returning empty sidecar status")
+	if pod.Spec.Containers == nil {
+		klog.Infof("Pod Containers  was nil, returning not enough information sidecar status")
 		return NotEnoughInformationSidecarStatus
 	}
+	if pod.Status.ContainerStatuses == nil {
+		for _, container := range pod.Spec.Containers {
+			if isSidecar(pod, container.Name) {
+				klog.Infof("Pod Containers Status was nil, sidecar is present: returning not enough information status")
+				return NotEnoughInformationSidecarStatus
+			}
+		}
+		klog.Infof("Pod Containers Status was nil, no sidecar present: returning empty sidecar status")
+		return SidecarsStatus{}
+	}
+
 	sidecarsStatus := SidecarsStatus{SidecarsPresent: false, SidecarsReady: true, ContainersWaiting: true}
 	for _, container := range pod.Spec.Containers {
 		for _, status := range pod.Status.ContainerStatuses {
@@ -939,4 +950,8 @@ func GetSidecarsStatus(pod *v1.Pod) SidecarsStatus {
 		}
 	}
 	return sidecarsStatus
+}
+
+func isSidecar(pod *v1.Pod, containerName string) bool {
+	return pod.Annotations[fmt.Sprintf("sidecars.lyft.net/container-lifecycle-%s", containerName)] == "Sidecar"
 }